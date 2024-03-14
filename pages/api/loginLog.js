06a0495a52f6873b74df185474d8389444e3a3ca
unknown <yjisoo77@gmail.com>
2024-03-15 07:43:04 +0900

    0315

--- pages/api/loginLog.js
+++ pages/api/loginLog.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
     ip = req.connection.remoteAddress;
   }
 
-  return res.status(200).json({ ip: ip, session: session2 });
+  return res.status(200).json({ ip: ip, session: session, session2: session2 });
 
   const user = await db
     .collection("user_cred")
