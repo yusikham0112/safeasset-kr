2133bb4a268be7f19abc0ee8372c7aa8db91308f
unknown <yjisoo77@gmail.com>
2024-03-20 10:45:29 +0900

    middleware

--- middleware.js
+++ middleware.js
@@ -1,9 +1,11 @@
 import { getToken } from "next-auth/jwt";
+import { getSession } from "next-auth/react";
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
   const currentPath = request.nextUrl.pathname;
   const token = await getToken({ req: request });
+  const session = await getSession({ request });
   const pathList = [
     "/trade",
     "/deposit",
@@ -17,7 +19,9 @@ export async function middleware(request) {
 
   // 경로 배열을 순회하면서 로그인이 필요한 페이지인지 확인
   const requiresLogin = pathList.some((path) => currentPath.startsWith(path));
+  console.log(session);
   console.log(token);
+
   // 로그인이 필요한 페이지에 접근한 경우
   if (requiresLogin && !token) {
     console.log("Could not get token");
